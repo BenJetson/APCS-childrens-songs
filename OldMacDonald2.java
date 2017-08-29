13b47837145157ca504de6fb2ed72359dc76f46e
Ben Godfrey <BenJetson@users.noreply.github.com>
2017-08-29 09:02:57 -0400

    Added JavaDoc comments for OldMacDonald.

--- OldMacDonald2.java
+++ OldMacDonald2.java
@@ -11,25 +11,41 @@
 public class OldMacDonald2 
 {
     
+    /**
+     * Prints the first verse of the song.
+     */
     public static void eieio() 
     { 
         System.out.println("Old MacDonald had a farm, E I E I O.");
     }
 
-
+    /**
+     * Prints the verse about what animal the farmer has.
+     * 
+     * @param animal The name of the animal as a String.
+     */
     public static void hadA(String animal) 
     { 
         System.out.println("And on that farm he had a " + animal + ", E I E I O.");
     }
 
-
+    /**
+     * Prints the two verses about the sounds the animal makes.
+     * 
+     * @param sound An onomotopoeia that describes the animal's sound, as a String.
+     */
     public static void withA(String sound) 
     { 
         System.out.println("With a " + sound + " " + sound +  " here and a " + sound + " " + sound +  " there.");
         System.out.println("Here a " + sound + ", there a " + sound + ", everywhere a " + sound + ", " + sound + ".");  
     }
 
-    
+    /**
+     * Prints one full verse of the song using the other helper methods.
+     * 
+     * @param animal The name of the animal as a String.
+     * @param sound An onomatopoeia that describes the animal's sound, as a String. 
+     */
     public static void singIt(String animal, String sound) 
     {
         eieio();
@@ -39,7 +55,9 @@ public class OldMacDonald2
         System.out.println();
     }
 
-    
+    /**
+     * Makes calls to the singIt() method to sing the whole song.
+     */
     public static void main(String[] args) 
     {
         System.out.println("Old MacDonald Had A Farm\n"); // Give our        song a title
@@ -51,6 +69,9 @@ public class OldMacDonald2
         
         // now see how easy it is to add some more animals  
         
+        singIt("cat", "meow");
+        singIt("dog", "bark");
+        singIt("snake", "slither");
         
         // modify the methods above to get quotation marks around the animal sounds
     }
